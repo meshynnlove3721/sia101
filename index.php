5f4c817c1b2ffae249783502c39623783fbc267b
meshynnlove3721 <95574442+meshynnlove3721@users.noreply.github.com>
2021-12-09 23:13:13 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?PHP
+        echo "HELLO!";
+        echo "This is Meshynn";
+
+        <html>
+
+        <body>
+
+        <h1>INSTALLING THE DOCKER DESKTOP</h1>
+        <p> In installing Docker Desktop, I go to docker website then download the docker in windows</p>
+        <p>In settings of my windows feature i enable the WSL, andvirtual machine then i checkif my hyper V is enabled then tha virtualization is enable in task manager.</p>
+        <p> Then I install the ubuntu twenty point zero four, Then I check ifthe docker is already starting then I go to resources to checkif the ubuntu is enabled</p>
+        <p> Then ido all the commands here in cmd admin to makedirectoryand so on</p>
+        </html>
+        </body>
+?>
