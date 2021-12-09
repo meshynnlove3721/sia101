37a66045870d92cd9846b6407c43b2d45bcfd4d7
meshynnlove3721 <95574442+meshynnlove3721@users.noreply.github.com>
2021-12-10 01:07:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,11 +6,12 @@
 
         <body>
 
-        <h1>INSTALLING THE DOCKER DESKTOP</h1>
-        <p> In installing Docker Desktop, I go to docker website then download the docker in windows</p>
-        <p>In settings of my windows feature i enable the WSL, andvirtual machine then i checkif my hyper V is enabled then tha virtualization is enable in task manager.</p>
-        <p> Then I install the ubuntu twenty point zero four, Then I check ifthe docker is already starting then I go to resources to checkif the ubuntu is enabled</p>
+        <h1>INSTALLING THE DOCKER DESKTOP</h1><br>
+        <p> In installing Docker Desktop, I go to docker website then download the docker in windows</p><br>
+        <p>In settings of my windows feature i enable the WSL, andvirtual machine then i checkif my hyper V is enabled then tha virtualization is enable in task manager.</p><br>
+        <p> Then I install the ubuntu twenty point zero four, Then I check ifthe docker is already starting then I go to resources to checkif the ubuntu is enabled</p><br>
         <p> Then ido all the commands here in cmd admin to makedirectoryand so on</p>
+                
         </html>
         </body>
 ?>
